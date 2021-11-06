49d32ff08744f6f3f214574337d6273f8020399d
Jesús Redondo García <redondogarciajesus@gmail.com>
2021-11-06 20:15:22 +0100

    First Character walking

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,30 @@ int main(int argc, char** argv) {
   params.argv = argv;
   params.vsync = 1;
   params.sdl_window_flags = SDL_WINDOW_RESIZABLE;
-  params.window_title = "Libtcod Template Project";
+  params.window_title = "Template Rouguelite";
 
-  tcod::ConsolePtr console = tcod::new_console(80, 25);
+  tcod::ConsolePtr console = tcod::new_console(80, 50);
   params.columns = console->w;
   params.rows = console->h;
 
   tcod::ContextPtr context = tcod::new_context(params);
 
+
+//Player:
+int playerX = 40;
+int playerY = 25;
+std::string playerChar = "@";
+
+
+
+
+
   // Game loop.
   while (true) {
     // Rendering.
     TCOD_console_clear(console.get());
-    tcod::print(*console, 0, 0, "Hello World", &TCOD_white, nullptr, TCOD_BKGND_SET, TCOD_LEFT);
+    //tcod::print(*console, 0, 0, "Hello World", &TCOD_white, nullptr, TCOD_BKGND_SET, TCOD_LEFT);
+    tcod::print(*console, playerX, playerY, playerChar, &TCOD_white, nullptr, TCOD_BKGND_SET, TCOD_LEFT);
     context->present(*console);
 
     // Handle input.
@@ -31,6 +42,32 @@ int main(int argc, char** argv) {
     SDL_WaitEvent(nullptr);
     while (SDL_PollEvent(&event)) {
       switch (event.type) {
+        case SDL_KEYDOWN:
+          switch (event.key.keysym.sym)
+          {
+            case SDLK_RIGHT:
+              playerX++;
+              break;
+
+            case SDLK_LEFT:
+              playerX--;
+              break;
+
+            case SDLK_UP:
+              playerY--;
+              break;
+
+            case SDLK_DOWN:
+              playerY++;
+              break;
+
+
+            default:
+              break;
+          }
+
+
+          break;
         case SDL_QUIT:
           std::exit(EXIT_SUCCESS);
           break;
